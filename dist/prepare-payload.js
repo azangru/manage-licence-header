32e9a52f4fa2bdc0efb0e1d347f662f6d73559d1
Andrey Azov <andrey@ebi.ac.uk>
2020-03-19 10:18:09 +0000

    Update dist code

--- dist/prepare-payload.js
+++ dist/prepare-payload.js
@@ -2,6 +2,13 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = __importDefault(require("fs"));
 const paths_1 = require("./utils/paths");
@@ -10,7 +17,7 @@ const prepareRegularPayload = async (argv) => {
     let { config: configPath, template: templatePath } = argv;
     configPath = paths_1.getAbsolutePath(configPath);
     templatePath = paths_1.getAbsolutePath(templatePath);
-    const config = require(configPath);
+    const { default: config } = await Promise.resolve().then(() => __importStar(require(configPath)));
     const template = fs_1.default.readFileSync(templatePath, 'utf-8');
     const filePaths = await paths_1.getFilePaths(fileList);
     return {
